adda26cbe69012ada69765ee301757ee5d81812c
Farouq Taiwo <Farouqtaiwo45@gmail.com>
2026-01-17 15:34:25 +0100

    base.html modified

--- core/accounts/urls.py
+++ core/accounts/urls.py
@@ -1,7 +1,8 @@
+# accounts/urls.py
 from django.urls import path
-from django.contrib.auth import views as auth_views
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
-    path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
+    path('login/', LoginView.as_view(template_name='accounts/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(next_page='/'), name='logout'),
 ]
