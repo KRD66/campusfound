ed227334b4ba1f2fdfd90eb3a346df6144ba0041
Farouq Taiwo <Farouqtaiwo45@gmail.com>
2026-01-17 21:51:42 +0100

    registration templates added, login template modified

--- core/accounts/urls.py
+++ core/accounts/urls.py
@@ -1,8 +1,9 @@
-# accounts/urls.py
+
 from django.urls import path
-from django.contrib.auth.views import LoginView, LogoutView
+from . import views
 
 urlpatterns = [
-    path('login/', LoginView.as_view(template_name='accounts/login.html'), name='login'),
-    path('logout/', LogoutView.as_view(next_page='/'), name='logout'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('register/', views.register_view, name='register'),
 ]
